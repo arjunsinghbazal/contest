0f95549fab5a5c69bdd1c5da7d121bba097d874b
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-08-19 13:00:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { Provider } from 'react-redux';
 import store from './Redux/Store';
 
@@ -15,4 +15,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
+
